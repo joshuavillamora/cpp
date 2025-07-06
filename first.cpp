faf967f262899fe6ad170f9cd6e6c298eed480eb
joshuavillamora <earikjoshua@gmail.com>
2025-07-06 22:23:18 +0800

    Number Guessing Game using a do while loop and a Random Number Generator

--- first.cpp
+++ first.cpp
@@ -540,6 +540,8 @@ int main() {
     }
     */
 
+    // Rock Paper Scissors
+    /*
     int opp = rand() % 3 + 1;
     int oppHealth = 3;
     int health = 3;
@@ -599,6 +601,29 @@ int main() {
         std::cout << "Pick a number between 1-3.";
         break;
     }
+    */
+
+    int num;
+    int guess;
+    int tries = 0;
+
+    srand(time(NULL));
+
+    num = (rand() % 100) + 1;
+    std::cout << "********** NUMBER GUESSING GAME **********\n";
+    do {
+        std::cout << "Enter a guess between (1-100): ";
+        std::cin >> guess;
+        tries++;
+        if (guess > num) {
+            std::cout << "Too high! Try again. \n";
+        } else if (guess < num) {
+            std::cout << "Too low! Try again. \n";
+        } else {
+            std::cout << "Congratulations! It took you " << tries << " attempts to guess the number " << num << ".";
+        }
+    } while (guess != num);
+    std::cout << "\n******************************************";
 
     return 0;
 }
